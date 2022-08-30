42757e45b8720a7695e2c38b5af97a3dcc3f1a2b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-08-31 00:06:17 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/React-Design-System/react-main">
+            to="/docs/React-Design-System">
             React-Design-System
           </Link>
         </div>
